dce4d2ac719f88d9c3740d6319f4c275ec5f0d58
CraigGeng <841082723@qq.com>
2019-09-15 17:41:37 +0800

    add check cookies

--- src/main/java/com/craiggeng/community/community/Mapper/UserMapper.java
+++ src/main/java/com/craiggeng/community/community/Mapper/UserMapper.java
@@ -4,6 +4,8 @@ package com.craiggeng.community.community.Mapper;
 import com.craiggeng.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -11,4 +13,6 @@ public interface UserMapper {
     @Insert("insert into user (NAME,ACCOUNT_ID,TOKEN,GMT_CREATE,GMT_MODIFIED) values (#{name} , #{accountId} , #{token} , #{gmtCreate} , #{gmtModified})")
     void insert(User user);
 
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
